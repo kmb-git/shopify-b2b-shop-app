bfff4a55183d4419d08c1e646052e0c6a63d962b
Farukh Sultan <farukh.5937@gmail.com>
2025-01-03 16:39:08 +0500

    CORS UPDATE

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var listRouter = require("./routes/listRoutes"); // Handles list page rendering
 var authRouter = require("./routes/auth"); // Authentication routes
 var protectedRouter = require("./routes/protected"); // Other protected routes
 var priceRouter = require("./routes/priceRules"); // Other protected routes
+var cors = require("cors");
 
 var app = express();
 databaseConfig(); // Initialize database connection
@@ -34,6 +35,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(cors());
 // Session management configuration
 app.use(
   session({
