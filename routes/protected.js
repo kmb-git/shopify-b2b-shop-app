50c56435b8d64f6be4237ec623f01448f4e3a58f
Farukh Sultan <farukh.5937@gmail.com>
2025-01-03 21:23:49 +0500

    HOMEPAGE INDEX ISSUE

--- routes/protected.js
+++ routes/protected.js
@@ -2,6 +2,9 @@ const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
+router.get("/", authMiddleware, (req, res) => {
+  res.redirect("/home");
+});
 router.get("/protected", authMiddleware, (req, res) => {
   res.render("protected", { title: "Protected Page", user: req.session.user });
 });
